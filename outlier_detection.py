e65a238c0b44f94763dc2f703f0f379574896aa6
Arjun Surendran <arjun.surendran@yahoo.in>
2019-11-26 21:34:26 -0500

    Bug fixes

--- outlier_detection.py
+++ outlier_detection.py
@@ -20,6 +20,10 @@ def collect_args():
     data_files = glob.glob(data_folder + "/*.txt")
     return data_files, [low, high]
 
+def read_threshold_from_file(filename):
+    df = pd.read_csv(filename, sep="\t", index_col = 0, squeeze=True, header=None)
+    return df
+    
 def find_outliers():
     data_files, thresholds = collect_args()
     df_list = []
@@ -37,27 +41,44 @@ def find_outliers():
     data = pd.concat(df_list)
     data = data.fillna(0)
 
-    ids = data['ROI_ID']
-    data = data.drop('ROI_ID', axis=1)
-
     low, high = thresholds
-    Q1 = data.quantile(low)
-    Q3 = data.quantile(high)
 
-    Q1.to_csv('lower_threshold.txt', sep='\t')
-    Q3.to_csv('higher_threshold.txt', sep='\t')
+    ids = data['ROI_ID'].unique()
+    for id in ids:
+        
+        df = data[data['ROI_ID'] == id]
+        df.drop('ROI_ID', axis=1)
+        
+        low_file_name = "./thresholds/" + str(id) + "_" + str(low) + "_lower_threshold.txt"
+        high_file_name = "./thresholds/" + str(id) + "_" + str(high) +  "_higher_threshold.txt"
+        
+        try:
+            Q1 = read_threshold_from_file(low_file_name)
+            Q3 = read_threshold_from_file(high_file_name)
 
-    Out1 = data[data > Q3]
-    Out2 = data[data < Q1]
+        except FileNotFoundError:
+            Q1 = df.quantile(low)
+            Q3 = df.quantile(high)
+            Q1.to_csv(low_file_name, sep="\t")
+            Q3.to_csv(high_file_name, sep="\t")
 
-    Out1 = Out1.dropna('rows', how='any')
-    Out2 = Out2.dropna('rows', how='any')
+        Out1 = data[df >= Q3]
+        Out2 = data[df <= Q1]
 
-    outlier_ids =  [ids[Out1.index], ids[Out2.index]]
-    
-    outliers = pd.concat(outlier_ids)
+        Out1 = Out1.dropna('rows', how='any')
+        Out2 = Out2.dropna('rows', how='any')
+        
+        out_list = [Out1, Out2]
+        out = pd.concat(out_list)
+        
+        outfile = "./outliers/" + str(id) + ".txt"
 
-    outliers.to_csv('outlier_ids.txt', sep='\t', index=False)
+        if out.empty:
+            with open(outfile, "w") as outfile:
+                outfile.write("No Outliers!")
+        else:
+            print(id)
+            out.to_csv(outfile, sep="\t")
 
 if __name__ == '__main__':
     find_outliers()
